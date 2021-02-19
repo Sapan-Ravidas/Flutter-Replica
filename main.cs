d873760cb8f68ce776dfe9804a1ebc53afd8ae5c
Sapan-Ravidas <sapanravidas@gmail.com>
2021-02-19 22:19:04 +0530

    start

---
+++ main.cs
@@ -0,0 +1,65 @@
+body {
+    margin: 0;
+    font-family: "Roboto", sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #4A4A4A;
+    text-align: left;
+    background-color: #fff;
+}
+
+.home-page {
+    background-color: #F8F9FA;
+}
+
+.site-header {
+    background-color: #FFF;
+    box-shadow: 0px 2px 4px rgba(0,0,0,0.1);
+    font-family: "Google Sans", "Roboto", sans-serif;
+    position: -webkit-sticky;
+    position: sticky;
+    top: 0;
+    z-index: 100;
+}
+
+.site-header .navbar {
+    font-size: 1.25rem;
+    min-height: 66px;
+}
+
+.justify-content-start {
+    justify-content: flex-start !important;
+}
+
+.navbar {
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.5rem 1rem;
+}
+
+nav, section{
+    display: block;
+}
+
+.navbar-toggler {
+    padding: 0.25rem 0.75rem;
+    font-size: 1.25rem;
+    line-height: 1;
+    background-color: transparent;
+    border: 1px solid transparent;
+    border-radius: 0;
+}
+
+.navbar-brand {
+    display: inline-block;
+    padding-top: 0.3125rem;
+    padding-bottom: 0.3125rem;
+    margin-right: 1rem;
+    font-size: 1.25rem;
+    line-height: inherit;
+    white-space: nowrap;
+}
